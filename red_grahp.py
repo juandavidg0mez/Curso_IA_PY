365f93dead616834a5638e91b6a62ce977bff07f
juandavidg0mez <nihlgomez@gmail.com>
2026-01-13 15:18:09 -0500

    Script Terminado

--- red_grahp.py
+++ red_grahp.py
@@ -7,11 +7,22 @@ def identify_node(graph):
         dict_parse_count_graph[node] = list(edges)
     return dict_parse_count_graph
 
-def identify_arista(graph):
-    pass
 
-def count_comications(graph):
-    pass
+def count_concurrency(graph):
+    concurrency = {node: 0 for node in graph}
+
+    for node, edges in graph.items():
+        # Aristas salientes
+        concurrency[node] += len(edges)
+
+        # Aristas entrantes
+        for target in edges:
+            concurrency[target] += 1
+
+    return concurrency
+
 
 test_graph = "A:BCD;B:;C:D;D:BC"
 print(identify_node(test_graph))
+
+print(count_concurrency(dict_parse_count_graph))
